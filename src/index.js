4413549b6f12bf65c62932ee0de8c679ff7a344a
Quwam Binuyo <qbinuyo7@gmail.com>
2025-02-12 11:09:21 +0100

    NEst component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const Person = () => <h2>John Doe</h2>;
+const Message = () => {
+  return <p>This is my message</p>;
+};
+
 function Greeting() {
-  return <h2>My First Component</h2>;
+  return (
+    <>
+      <div>
+        <Person />
+        <Message />
+      </div>
+    </>
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
