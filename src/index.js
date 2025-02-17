041ad0a92297e7a0685bc8132bfd98cdd7e4d64d
Quwam Binuyo <qbinuyo7@gmail.com>
2025-02-17 16:29:08 +0100

    event basics

--- src/index.js
+++ src/index.js
@@ -27,14 +27,41 @@ const books = [
 // BookList component - renders a list of books
 const BookList = () => {
   return (
-    <section className="booklist">
-      {/* Iterating through the books array using map() */}
-      {books.map((book) => {
-        console.log(book); // Logs each book object to the console for debugging
+    <>
+      <EventExample />
+      <section className="booklist">
+        {/* Iterating through the books array using map() */}
+        {books.map((book) => {
+          console.log(book); // Logs each book object to the console for debugging
 
-        // Spreading book properties into the Book component and using id as a unique key
-        return <Book {...book} key={book.id} />;
-      })}
+          // Spreading book properties into the Book component and using id as a unique key
+          return <Book {...book} key={book.id} />;
+        })}
+      </section>
+    </>
+  );
+};
+
+const EventExample = (e) => {
+  const handleFormInput = () => {
+    console.log("handle form input");
+  };
+  const handleButtonClick = (e) => {
+    console.log("handle click");
+  };
+
+  return (
+    <section>
+      <form>
+        <h2>typical form</h2>
+        <input
+          type="text"
+          name="example"
+          style={{ margin: "1rem" }}
+          onChange={handleFormInput}
+        />
+      </form>
+      <button onClick={handleButtonClick}>Click me</button>
     </section>
   );
 };
