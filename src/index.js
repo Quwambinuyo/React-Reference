7363cb788f55c2e5a16cac9063afac813d75f117
Quwam Binuyo <qbinuyo7@gmail.com>
2025-02-17 12:47:42 +0100

    still on props

--- src/index.js
+++ src/index.js
@@ -21,10 +21,10 @@ const Book = (props) => {
 
   return (
     <article className="book">
-      <img src={image} alt={title} />
+      <img src={props.image} alt={props.title} />
 
-      <h2>{title}</h2>
-      <h4>{author.toUpperCase()}</h4>
+      <h2>{props.title}</h2>
+      <h4>{props.author.toUpperCase()}</h4>
     </article>
   );
 };
