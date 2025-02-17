f534bcd3ea5bf2866d4929e920ab158bd05504c9
Quwam Binuyo <qbinuyo7@gmail.com>
2025-02-17 12:46:56 +0100

    still on props

--- src/index.js
+++ src/index.js
@@ -9,14 +9,16 @@ const image = "https://m.media-amazon.com/images/I/91I1KDnK1kL._SY385_.jpg";
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
+      <Book author={author} title={title} img={image} />
+      <Book author={author} title={title} img={image} />
       <Book />
     </section>
   );
 };
 
-const Book = () => {
+const Book = (props) => {
+  console.log(props);
+
   return (
     <article className="book">
       <img src={image} alt={title} />
